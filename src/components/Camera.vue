fbdc5aec1be93f4203667e3ba44debef6a8f2871
Tristan Teufel <tristanteufel@googlemail.com>
2017-03-26 17:26:09 +0200

    better storage handling

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -50,7 +50,7 @@
         }
       },
       findObjectByPropertyInArray(nameKey, myArray){
-        for (var i = 0; i < myArray.length; i++) {
+        for (let i = 0; i < myArray.length; i++) {
           if (myArray[i].url === nameKey) {
             return myArray[i];
           }
